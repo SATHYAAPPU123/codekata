f2bc6b21c2f54a258f677d268bddee54f43aa83e
SATHYAAPPU123 <36396327+SATHYAAPPU123@users.noreply.github.com>
2018-03-23 12:09:54 +0530

    Create positive.cpp

---
+++ positive.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cout<<"enter the number"<<endl;;
+	cin>>n;
+	if(n>0)
+	
+	{
+		cout<<"the number is positive"<<endl;
+		
+	}
+	else if(n<0)
+	{
+		cout<<"the num is negative"<<endl;
+	}
+	else
+	{
+		cout<<"the num is zero"<<endl;
+	}
+	return 0;
+}
